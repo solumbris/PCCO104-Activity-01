c3078c60531e8652330c54aff8da564339b9a546
jewellapuz1205 <31803787-jewellapuz1205@users.noreply.replit.com>
2024-02-03 02:51:40 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Jewel";
+    Console.WriteLine ($"Hello World {firstName}");
+    Console.WriteLine ("Hello World " + firstName);
+    
+  }
+}
